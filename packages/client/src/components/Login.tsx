4d3503eb0041ccc3dac8fe6e7187a90da7ff6320
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2022-02-04 23:03:14 +0100

    working on client:login

--- packages/client/src/components/Login.tsx
+++ packages/client/src/components/Login.tsx
@@ -7,30 +7,44 @@ import {
 	Typography,
 } from '@mui/material';
 import { Box } from '@mui/system';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { AxiosError } from 'axios';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
+import { ILoginInput, loginReq } from '../common/requests';
 
 const Login = () => {
-	const [data, setData] = useState(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setError,
+	} = useForm<ILoginInput>();
+
+	const onError = (err: AxiosError) => {
+		const errMsg = err?.response?.data.error;
+		if (errMsg === 'wrong password or username') {
+			setError('username', { message: errMsg });
+			setError('password', { message: errMsg });
+		} else {
+			setError('username', { message: err.message });
+			setError('password', { message: err.message });
+		}
+	};
+
+	const { data, mutate, isLoading, isSuccess, reset } = useMutation(loginReq, {
+		onError,
+	});
+
+	const onSubmit = (user: ILoginInput) => {
+		mutate(user);
+	};
 
 	useEffect(() => {
-		const loginReq = async () => {
-			const res = await axios.post(
-				'http://localhost:4000/api/users/login',
-				{
-					username: 'user1',
-					password: 'password',
-				},
-				{
-					withCredentials: true,
-				}
-			);
-			setData(res.data);
-		};
-		loginReq();
-	}, [setData]);
-
-	console.log(data);
+		if (isSuccess && data) {
+			localStorage.setItem('currentUser', JSON.stringify(data));
+		}
+	}, [data, isSuccess]);
 
 	return (
 		<Stack
@@ -57,11 +71,26 @@ const Login = () => {
 				</Typography>
 			</Box>
 			<Paper sx={{ p: '20px', width: '400px' }}>
-				<Stack spacing={2}>
-					<TextField id='email' label='Email' variant='outlined' />
-					<TextField id='password' label='Password' variant='outlined' />
-					<Button variant='contained' size='large'>
-						Log In
+				<Stack spacing={2} component='form' onSubmit={handleSubmit(onSubmit)}>
+					<TextField
+						id='email'
+						label='Email'
+						variant='outlined'
+						{...register('username')}
+					/>
+					<TextField
+						id='password'
+						label='Password'
+						variant='outlined'
+						{...register('password')}
+					/>
+					<Button
+						variant='contained'
+						size='large'
+						type='submit'
+						disabled={isLoading}
+					>
+						{isLoading ? 'in progress' : 'log in'}
 					</Button>
 					<Button variant='text' size='small'>
 						Forget password?
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
 	<React.StrictMode>
 		<RecoilRoot>
 			<BrowserRouter>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
 			</BrowserRouter>
 		</RecoilRoot>
 	</React.StrictMode>,
